a8263591219f0a4e0cbed0099dc5e421f6e49f48
aznarjorge <jorgeaznar_97@hotmail.com>
2018-05-11 00:06:14 +0200

    V.Jorge

---
+++ cmake-build-debug/CMakeFiles/Algoritmia_Basica.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Algoritmia_Basica.dir/Algoritmo.cpp.obj"
+  "CMakeFiles/Algoritmia_Basica.dir/Frontera.cpp.obj"
+  "CMakeFiles/Algoritmia_Basica.dir/main.cpp.obj"
+  "CMakeFiles/Algoritmia_Basica.dir/Nodo.cpp.obj"
+  "CMakeFiles/Algoritmia_Basica.dir/Generador.cpp.obj"
+  "Algoritmia_Basica.pdb"
+  "Algoritmia_Basica.exe"
+  "Algoritmia_Basica.exe.manifest"
+  "libAlgoritmia_Basica.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Algoritmia_Basica.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
